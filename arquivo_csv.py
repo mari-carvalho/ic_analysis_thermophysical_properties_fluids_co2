94b08b6f6ca6522fe01426629d52f181d98f6c3f
03950025081 <dide.carvalho@hotmail.com>
2024-09-25 16:31:23 -0300

    2

--- arquivo_csv.py
+++ arquivo_csv.py
@@ -3,13 +3,13 @@ import pandas as pd
 import openpyxl
 import csv
 
-file_path = r'C:\Users\03950025081\Desktop\Simulações Thermobuilder\CH4+CO2\Metan Databank.xlsx'
+file_path = r'C:\Users\03950025081\Desktop\Metan Databank.xlsx'
 
 workbook = openpyxl.load_workbook(file_path, data_only=True) # carregar a planilha com data_only=True para ler os resultados das fórmulas
 
-sheet = workbook.worksheets[0] # selecionar apenas a primeira aba
+sheet = workbook.worksheets[2] # selecionar apenas a primeira aba
 
-caminho_novo = r'C:\Users\03950025081\Desktop\Simulações Thermobuilder\CH4+CO2\Metan Databank.csv' # caminho onde será salvo o novo arquivo
+caminho_novo = r'C:\Users\03950025081\Desktop\Metan Databank.csv' # caminho onde será salvo o novo arquivo
 
 with open(caminho_novo, mode='w', newline='', encoding='utf-8') as file_csv: # abre o arquivo para escrita ou para ser sobrescrito (se já existir); newline evita a inserção de linhas em branco; encoding='utf-8' define a codificação do arquivo para suportar caracteres especiais; as file_csv garante que será aberto e fechado ao final do bloco
     writer = csv.writer(file_csv, delimiter=';') # cria um objeto writer usado para escrever no arquivo csv
--- ponto_virgula.py
+++ ponto_virgula.py
@@ -1,13 +1,23 @@
 import pandas as pd
+from openpyxl import load_workbook
 
-file_path = r'C:\Users\03950025081\Desktop\Simulações Thermobuilder\CH4+CO2\Metan Databank.csv'
+file_path = r'C:\Users\03950025081\Desktop\Metan Databank.csv'
 
-df = pd.read_csv(file_path, delimiter=';', encoding='utf-8')
+df = pd.read_csv(file_path, delimiter=';', encoding='utf-8', dtype=str)
 
-for column in df.select_dtypes(include=['float64', 'int64']).columns:
-    df[column] = df[column].astype(str).str.replace('.', ',', regex=False)
+def corrigir_pontos(valor):
+    if '.' in valor:
+        partes = valor.split('.')
+        if len(partes[0]) > 3:
+            valor_corrigido = float(valor.replace('.', '')) / 1000
+            return f"{valor_corrigido: .2f}".replace('.', ',')
+        else:
+            return valor.replace('.', ',')
+    return valor
 
-caminho_novo_csv = r'C:\Users\03950025081\Desktop\Simulações Thermobuilder\CH4+CO2\Metan Databank Modificado.csv'
+df = df.applymap(corrigir_pontos)
+
+caminho_novo_csv = r'C:\Users\03950025081\Desktop\Metan Databank Modificado Modificado.csv'
 df.to_csv(caminho_novo_csv, index=False, sep=';', encoding='utf-8')
 
 
